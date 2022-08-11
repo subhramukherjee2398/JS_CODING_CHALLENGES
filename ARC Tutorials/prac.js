54b6c8126ceabe93239c1a78a50c05b5f0de0eea
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-08-11 18:28:47 +0530

    extra

--- ARC Tutorials/prac.js
+++ ARC Tutorials/prac.js
@@ -1,5 +1,17 @@
 
-var num = [1, 2, 3, 6, 7, 8];
-(num.splice(2,2,100,200))
-console.log(num)
 
+function check(s){
+    let a = ["a", "e", "i", "o", "u"]
+    let x = s.split("")
+    let letters = []
+ //   console.log(x)
+    x.map((ele)=>{
+        if(a.includes(ele)){
+          //  console.log(ele)
+            letters.push(ele)
+        }
+    })
+    console.log(letters)
+}
+
+check("aaaajkkkkol")
--- Veteran Code/Qns9.js
+++ Veteran Code/Qns9.js
@@ -12,3 +12,20 @@ const countvowel = (str) => {
 };
 
 console.log(countvowel("aaaammmm"));
+
+// my solution :
+function check(s){
+  let a = ["a", "e", "i", "o", "u"]
+  let x = s.split("")
+  let letters = []
+//   console.log(x)
+  x.map((ele)=>{
+      if(a.includes(ele)){
+        //  console.log(ele)
+          letters.push(ele)
+      }
+  })
+  console.log(letters)
+}
+
+check("aaaajkkkkol")
